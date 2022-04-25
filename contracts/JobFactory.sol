1124f26318fd9e74fb2c6796f8993438645948e7
hannah-steele <55158545+hannah-steele@users.noreply.github.com>
2022-04-25 16:00:59 -0400

    Revert "Temp push to be reverted -- initial thoughts on JobFactory.sol"
    This reverts commit 1c46b967517bb30d2c1bb60ffe9536695523f6d4.

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -9,28 +9,19 @@ import "./OrganizationManager.sol";
 contract JobFactory is OrganizationManager{
 
     event JobPosted(uint id);
-    event ApplicationCompleted();
-    event ApplicantAccepted();
-    event JobAssigned();
-    event ProgressUpdated();
-    event ProgressApproved();
-    event JobCompleted();
 
     ///@dev so far a job can have at max 255 hours 
     struct Job {
         string name;
         //uint64 id; id will be given through index in array
         uint64 budget; //in wei?
-        uint8 time; //time to complete the job in hours
+        uint8 time; //in hours
         uint8 percentCompleted;
         bool completed;
     }
 
     //uint64 private idCount;
     Job[] public jobs;
-    mapping(uint=>address[]) internal applicants; //maps job id to applicants for the job
-    mapping(uint=>address) internal approved; //maps job id to approved applicant
-    mapping(uint=>address) internal assignment; //maps job id to assignee
 
     constructor() {
         //idCount = -1;
@@ -38,10 +29,7 @@ contract JobFactory is OrganizationManager{
 
     ///@dev internal function creating jobs for potential calls in other places
     function _createJob() internal {
-    }
 
-    ///@notice checks that owner is good for budget and witholds the budget
-    function _withholdBudget() internal {
     }
 
     ///@notice creates a new job posting
@@ -50,24 +38,6 @@ contract JobFactory is OrganizationManager{
     function createJob(string memory _name, uint64 _budget, uint8 _time) public onlyOwner() {
         //idCount++;
         jobs.push(Job(_name, _budget, _time, 0, false));
-        // call withholdBudget here
         emit JobPosted(jobs.length-1);
     }
-
-    ///@notice apply to accept a job
-    function applyToJob() public {
-        emit ApplicationCompleted();
-    }
-
-    //@notice accept job Applicant
-    function acceptApplicant() public onlyOwner() {
-        emit ApplicantAccepted();
-    }
-
-    ///@notice accept job assignment
-    ///@dev jobs can only be accepted by approved applicants -- maybe implement via modifier.
-    function acceptAssignment() public { 
-        emit JobAssigned();
-    }
-
 }
