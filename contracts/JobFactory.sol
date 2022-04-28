ca1095ab1c1e1064a8d17d3e4fadcad2c136ad36
hsteele-columbia <81546941+hsteele-columbia@users.noreply.github.com>
2022-04-27 22:36:49 -0400

    Revert "temp updates to JobFactory.sol"
    This reverts commit 67dc169e678a21d6a9f5908908774a37717eb639.

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -8,10 +8,36 @@ import "./OrganizationManager.sol";
 
 contract JobFactory is OrganizationManager{
 
-    uint32[] jobIds;
-    uint32[] 
-    mapping (address => uint32[]) ownerJobs
-    mapping (address => uint32[]) workerJobs
-    
+    event JobPosted(uint id);
 
+    ///@dev so far a job can have at max 255 hours 
+    struct Job {
+        string name;
+        //uint64 id; id will be given through index in array
+        uint64 budget; //in wei?
+        uint8 time; //in hours
+        uint8 percentCompleted;
+        bool completed;
+    }
+
+    //uint64 private idCount;
+    Job[] public jobs;
+
+    constructor() {
+        //idCount = -1;
+    }
+
+    ///@dev internal function creating jobs for potential calls in other places
+    function _createJob() internal {
+
+    }
+
+    ///@notice creates a new job posting
+    ///@dev jobs can only be created by the owner
+    ///@dev incrementing idCount depends on overflow protection; only use with Solidity verions >0.8!
+    function createJob(string memory _name, uint64 _budget, uint8 _time) public onlyOwner() {
+        //idCount++;
+        jobs.push(Job(_name, _budget, _time, 0, false));
+        emit JobPosted(jobs.length-1);
+    }
 }
