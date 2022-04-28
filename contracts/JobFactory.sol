326f3ee0fafb96fec5cee46d2d4e14bd1bf923c7
hsteele-columbia <81546941+hsteele-columbia@users.noreply.github.com>
2022-04-27 23:00:26 -0400

    Update JobFactory.sol

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -10,6 +10,7 @@ contract JobFactory is OrganizationManager{
 
     event JobPosted(uint id);
     event ApplicationCompleted();
+    event ApplicationWithdrawn();
     event ApplicantAccepted();
     event JobAssigned();
     event ProgressUpdated();
@@ -19,21 +20,26 @@ contract JobFactory is OrganizationManager{
     ///@dev so far a job can have at max 255 hours 
     struct Job {
         string name;
-        //uint64 id; id will be given through index in array
+        uint64 id; //id will be given through index in array -- tbd but we may not actually need this in the job
         uint64 budget; //in wei?
         uint8 time; //time to complete the job in hours
         uint8 percentCompleted;
         bool completed;
     }
 
-    //uint64 private idCount;
+    uint64 private idCount;
+
     Job[] public jobs;
-    mapping(uint=>address[]) internal applicants; //maps job id to applicants for the job
-    mapping(uint=>address) internal approved; //maps job id to approved applicant
-    mapping(uint=>address) internal assignment; //maps job id to assignee
+
+    mapping(uint64=>address[]) internal applicants; //maps job id to applicants for the job
+    mapping(uint64=>address) internal approved; //maps job id to approved applicant
+    mapping(uint64=>address) internal assignment; //maps job id to assignee
+
+    mapping (address => uint64[]) ownerJobs
+    mapping (address => uint64[]) workerJobs
 
     constructor() {
-        //idCount = -1;
+       idCount = 0;
     }
 
     ///@dev internal function creating jobs for potential calls in other places
@@ -48,19 +54,27 @@ contract JobFactory is OrganizationManager{
     ///@dev jobs can only be created by the owner
     ///@dev incrementing idCount depends on overflow protection; only use with Solidity verions >0.8!
     function createJob(string memory _name, uint64 _budget, uint8 _time) public onlyOwner() {
-        //idCount++;
-        jobs.push(Job(_name, _budget, _time, 0, false));
+        idCount++;
+        jobs.push(Job(_name, idCount, _budget, _time, 0, false));
         // call withholdBudget here
         emit JobPosted(jobs.length-1);
     }
 
     ///@notice apply to accept a job
-    function applyToJob() public {
+    function applyToJob(uint64 _id) public {
+        applicants[_id] = msg.sender
         emit ApplicationCompleted();
     }
 
+    ///@notice apply to accept a job
+    function viewApplicants(uint64 _id) public view onlyOwner() returns (address[] _applicants) {
+        return applicants[_id];
+    }
+
     //@notice accept job Applicant
-    function acceptApplicant() public onlyOwner() {
+    function acceptApplicant(uint64 _id, address _applicant) public onlyOwner() {
+        // TO CODE: require( _applicant in applicants[_id])
+        approved[_id] = _applicant
         emit ApplicantAccepted();
     }
 
