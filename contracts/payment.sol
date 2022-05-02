ca2a733ab2f60ebf2798608013dcf31eca104a84
shariquemerchant <87384894+shariquemerchant@users.noreply.github.com>
2022-05-02 19:18:05 -0400

    Add files via upload

---
+++ contracts/payment.sol
@@ -0,0 +1,109 @@
+//OBJECTIVE
+//1. MONEY IS TAKEN FROM THE OWNER OF THE JOB AND SET INTO THIS CONTRACT
+//2. MONEY CAN ONLY BE WITHDRAWN BY THE CONTRACTOR ON SUCESSFUL COMPLETION OF THE JOB
+
+/**
+ *Ref:https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol
+ * @title Payment
+ * Payment follows a _pull payment_ model i.e. payments are not automatically forwarded to the
+ * accounts but kept in this contract, and the actual transfer is triggered as a separate step by calling the withdraw
+ * function.
+ */
+
+contract Payment {
+    event PaymentReceived(address from, uint256 amount);
+    event PaymentReleased(address to, uint256 amount);
+
+    //Will check later the view part of the variables and the reduncies of few varibales
+    uint64 public jobID;
+    address public contractoraddress;
+    uint64 public budget;
+    uint8 public percentCompleted;
+    bool public completed;
+
+    ///@notice creates a new instance of Payment
+    ///@dev jobs can only be created by the owner
+
+    constructor(
+        uint64 _jobID,
+        address _contractoraddress,
+        uint64 _budget,
+        uint8 _percentCompleted,
+        bool _completed
+    ) payable onlyOwner() {
+        jobID = _jobID; //Setting job ID
+        contractoraddress = _contractoraddress;
+        budget = _budget;
+        percentCompleted = _percentCompleted;
+        completed = _completed;
+        require(
+            msg.value == _budget,
+            "Owner should deposit money equal to the budget"
+        );
+
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Getter for the total budget for the job
+     */
+    function getBudget() public view returns (uint64) {
+        return budget;
+    }
+
+    /**
+     * @dev Getter for the progress for the job
+     */
+    function getprogress() public view returns (uint8) {
+        return percentCompleted;
+    }
+
+    /**
+     * @dev Getter for the completion of the job
+     */
+    function iscompleted() public view returns (bool) {
+        return percentCompleted == 1 ? true : false;
+    }
+
+    /**
+     * @dev Getter for the contract balance
+     */
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    /**
+     * @dev Function for contractor to withdraw assets if the job has been fulfilled
+     */
+    function _withdraw() public {
+        require(
+            msg.sender == contractoraddress,
+            "Only the contractor can withdraw the payment."
+        );
+
+        require(iscompleted() == true, "Job still not completed");
+        (bool paidContractor, ) = payable(contractoraddress).call{
+            value: address(this).balance
+        }("");
+        require(paidContractor, "Payment did not reach contractor");
+        emit PaymentReleased(msg.sender, address(this).balance);
+    }
+
+    //QUESIONS:
+    //1. Do we want to make payment if time taken is more than time limit. For that we need to introduce time componet
+    //2. Do we need a function to revert money back to the owner in case the job is not fulfilled.
+
+    //GENERAL GUIDELINES:
+    //ADD SOME MODIFIERS
+    //THINK ABOUT MORE FUNCTIONS
+    //PUBLIC VS PRIVATE, INTERNAL VS EXTERNAL
+    //IF FOR SOME REASON IF THE JOB IS NOT COMPLETED ADD SOME FUNCTIONALITY TO REVERT MONEY
+    //ADD FUNCTIONALITY TO UPDATE BUDGET AND % COMPLETED
+    //EMIT SOME EVENTS
+    //CHECK SOME PROPER IMPLEMENTAIONS ON YOUTUBE
+
+    //JOB FACTORY
+    //MSG.VALUE>BUDGET PAY THE REST
+    //CHECK TJE NAMING CONVENTION
+    //JOB ARRAY, JOB ID , CONTRCATOT ADDRESS
+}
