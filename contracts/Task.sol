98d85b7a613ece18ff624db270b4ff77fb0fa765
hsteele-columbia <81546941+hsteele-columbia@users.noreply.github.com>
2022-05-05 02:07:26 -0400

    Task + TaskFactory -- segregate logic appropriately between the two contracts

--- contracts/Task.sol
+++ contracts/Task.sol
@@ -4,133 +4,91 @@ pragma solidity ^0.8.13;
 import "./OrganizationManager.sol";
 
 
-///@title Collection and creation of all task postings
+///@title Task contracts
 
 contract TaskFactory is OrganizationManager{ // also probably is Payable or whatever our payments contract is
 
-    event TaskPosted(uint id);
-    event ApplicationCompleted(uint taskId, address applicant);
+    event ApplicationCompleted(address applicant);
     event ApplicationWithdrawn(uint taskId, address applicant);
     event ApplicantAccepted(uint taskId, address applicant);
-    event TaskAssigned(uint taskId, address assignee);
+    event Assignment(uint taskId, address assignee);
     event ProgressUpdated();
     event ProgressApproved();
-    event TaskCompleted();
+    event Completion();
 
-    string name;
-    string desc; // description of task
-    uint64 id; //id will be given through index in array -- tbd but we may not actually need this in the task
-    uint64 budget; //in wei?
-    uint8 time; //time to complete the task in hours: uint8 means task can have at most 255 hours
-    
-    uint8 percentCompleted;
+    // variables set at instantiation
+    string public name;
+    string public desc; // description of task
+    uint64 public id; //id will be given through index in array -- tbd but we may not actually need this in the task
+    uint64 public budget; //in wei?
+    uint8 public time; //time to complete the task in hours: uint8 means task can have at most 255 hours
 
-    bool completed;
+    // variables that are always set to the same value at instantiation
+    uint8 public percentCompleted;
+    bool public completed;
+    bool public isAssigned;
 
-    uint64 private idCount;
+    mapping(address=>bool) public isApplicant;
+    address[] public applicants;
+    address public approved;
+    address public assignment;
 
-    Task[] public tasks;
-    
-    // I don't actually think the below are needed -- use mappings to bool instead as we can loop through tasks array in a public view function that doesn't require gas.
-    // uint64[] completedTasks; // array to store past tasks that are already completed (e.g. for tax purposes, etc.)
-    // uint64[] ongoingTasks; // array to store ongoing tasks that are currently assigned
-    // uint64[] unassignedTasks; // array to store posted tasks that are unassigned, i.e. in need of assignment
-    // uint64[] assignedTasks; // array to store posted tasks that are unassigned, i.e. in need of assignment
 
-    // @Adrian: can't these information be held by a task contract? As we create them anyway that might be more memory efficient
-    mapping(uint64=>bool) isAssigned;
-    mapping(uint64=>bool) isComplete;
-    // @Adrian: mapping to an array... hopefully we keep track of the applicant id!
-    mapping(uint64=>address[]) internal applicants; // maps task id to applicants for the task
-    // @Adrian: why does task id have bytes32 and then uint64 format?
-    mapping(bytes32=>bool) internal isApplicant; // maps hash of task id and applicant address to boolean indicating whether the address corresponds to an applicant for the task
-    // @Adrian: probably again a mapping for the task contract?
-    mapping(uint64=>address) internal approved; // maps task id to a SINGLE approved applicant
-    mapping(uint64=>address) internal assignment; // maps task id to a SINGLE assignee
+    ///@notice constructor to instantiate the contract
+    constructor(string memory _name, string memory _desc, uint64 _id, uint64 _budget, uint8 _time) {
+        
+        // input variables
+        name = _name;
+        desc = _desc;
+        id = _id;
+        budget = _budget;
+        time = _time;
 
-    // @Adrian: this belongs here; managing tasks!
-    mapping (address => uint64[]) organizerTasks; // maps organizer address to tasks that they are an organizer of
-    mapping (address => uint64[]) workerTasks; // maps worker address to tasks that they are/were assigned to
+        // variables that are always the same at instantiation
+        percentCompleted = 0;
+        completed = 0;
+        isAssigned = 0;
+
+    }
 
     ///@notice only allow the task assignee to call the function
-    modifier onlyAssignee(_id) {
-        require(assignment[_id] == msg.sender, "Callable: caller is not the task assignee");
+    modifier onlyAssignee() {
+        require(assignment == msg.sender, "Callable: caller is not the task assignee");
         _;
     }
 
     ///@notice only allow an approved applicant to call the function
-    modifier onlyApproved(_id) {
-        require(approved[_id] == msg.sender, "Callable: caller's application has not been approved");
+    modifier onlyApproved() {
+        require(approved == msg.sender, "Callable: caller's application has not been approved");
         _;
     }
 
-    ///@notice constructor to instantiate the contract
-    ///@dev for now, all we have to do is set idCount to 0
-    constructor() {
-        // @Adrian: wouldn't id be implicitly managed through array position?
-       idCount = 0;
+    ///@notice apply for a task
+    function apply(address _applicant) public {
+        isApplicant[_applicant] = 1;
+        applicants.push(_applicant);
+        emit ApplicationCompleted(_applicant);
     }
 
-    /* TO DO */
-    ///@notice internal function creating tasks for potential calls in other places
-    ///@dev may not need in the end 
-    function _createTask() internal {
-    }
-
-    /* TO DO */
-    ///@notice checks that owner is good for budget and witholds the budget
-    ///@dev this may end up being done completely in the PaymentSplitter?
-    function _withholdBudget() internal { 
-    }
-
-    ///@notice creates a new task posting
-    ///@dev tasks can only be created by an organizer
-    ///@dev incrementing idCount depends on overflow protection; only use with Solidity verions >0.8!
-    function createTask(string memory _name, uint64 _budget, uint8 _time) public onlyOrganizer() {
-        idCount++;
-        tasks.push(Task(_name, idCount, _budget, _time, 0, false));
-        organizerTasks[msg.sender].push(idCount);
-        // call withholdBudget here
-        emit TaskPosted(tasks.length-1);
-    }
-
-    ///@notice view ALL task postings, past and present
-    function viewAllPostings() public view returns(Task[] tasks){
-        return tasks;
-    }
-
-    ///@notice view open postings that have not been assigned yet
-    function viewOpenPostings() public view returns(Task[] unassigned_tasks){
-        //for task in 
-    }
-
-    ///@notice apply to accept a task
-    function applyToTask(uint64 _id) public {
-        applicants[_id] = msg.sender;
-        keccak256(abi.encode(_id, msg.sender))] == 1
-        emit ApplicationCompleted(_id, msg.sender);
-    }
-
-    ///@notice view applicants for a task
+    ///@notice view applicants for the task
     ///@dev only an organizer of the task can do so
     ///@dev public view function -- no gas needed
-    function viewApplicants(uint64 _id) public view onlyOrganizer() returns (address[] _applicants) {
-        return applicants[_id];
+    function viewApplicants() public view onlyOrganizer() returns (address[] _applicants) {
+        return applicants;
     }
 
     //@notice accept task Applicant
-    function acceptApplicant(uint64 _id, address _applicant) public onlyOrganizer() {
-        require(isApplicant[keccak256(abi.encode(_id, _applicant))] == 1, "Callable: input address is not an existing applicant and therefore cannot be accepted.");
-        approved[_id] = _applicant;
-        emit ApplicantAccepted(_id, _applicant);
+    function acceptApplicant(address _applicant) public onlyOrganizer() {
+        require(isApplicant[_applicant] == 1, "Callable: input address is not an existing applicant and therefore cannot be accepted.");
+        approved = _applicant;
+        emit ApplicantAccepted(_applicant);
     }
 
     ///@notice accept task assignment
     ///@dev tasks can only be accepted by approved applicants
-    function acceptAssignment(uint64 _id) public onlyApproved(_id) { 
-        assignment[_id] = msg.sender;
-        workerTasks[msg.sender].push(_id);
-        emit TaskAssigned(_id, msg.sender);
+    function acceptAssignment(uint64 _address) public onlyApproved() { 
+        assignment = _address;
+        emit Assignment(_address);
     }
 
 }
--- contracts/TaskFactory.sol
+++ contracts/TaskFactory.sol
@@ -2,35 +2,33 @@
 pragma solidity ^0.8.13;
 
 import "./OrganizationManager.sol";
+import "./Task.sol";
 
 
 ///@title Collection and creation of all task postings
 
-contract TaskFactory is OrganizationManager{ // also probably is Payable or whatever our payments contract is
+contract TaskFactory is OrganizationManager{ 
 
     event TaskPosted(uint id);
-    event ApplicationCompleted(uint taskId, address applicant);
-    event ApplicationWithdrawn(uint taskId, address applicant);
-    event ApplicantAccepted(uint taskId, address applicant);
+    event TaskApplicationCompleted(uint taskId, address applicant);
+    event TaskApplicationWithdrawn(uint taskId, address applicant);
+    event TaskApplicantAccepted(uint taskId, address applicant);
     event TaskAssigned(uint taskId, address assignee);
-    event ProgressUpdated();
-    event ProgressApproved();
+    // event ProgressUpdated();
+    // event ProgressApproved();
     event TaskCompleted();
-
-    ///@dev so far a task can have at max 255 hours 
-    struct Task {
-        string name;
-        string desc; // description of task
-        uint64 id; //id will be given through index in array -- tbd but we may not actually need this in the task
-        uint64 budget; //in wei?
-        uint8 time; //time to complete the task in hours
-        uint8 percentCompleted;
-        bool completed;
-    }
 
     uint64 private idCount;
 
-    Task[] public tasks;
+    ///@notice constructor to instantiate the contract
+    ///@dev for now, all we have to do is set idCount to 0
+    constructor() {
+        // @Adrian: wouldn't id be implicitly managed through array position?
+        // @Hannah: yes, we can probably just manage ids through array push. but leaving it for now.
+       idCount = 0;
+    }
+
+    Task[] memory tasks;
     
     // I don't actually think the below are needed -- use mappings to bool instead as we can loop through tasks array in a public view function that doesn't require gas.
     // uint64[] completedTasks; // array to store past tasks that are already completed (e.g. for tax purposes, etc.)
@@ -39,97 +37,65 @@ contract TaskFactory is OrganizationManager{ // also probably is Payable or what
     // uint64[] assignedTasks; // array to store posted tasks that are unassigned, i.e. in need of assignment
 
     // @Adrian: can't these information be held by a task contract? As we create them anyway that might be more memory efficient
+    // @Hannah: we may not need these in the end but leaving the ones I'm not positive we DON"T need here for now.
     mapping(uint64=>bool) isAssigned;
     mapping(uint64=>bool) isComplete;
-    // @Adrian: mapping to an array... hopefully we keep track of the applicant id!
-    mapping(uint64=>address[]) internal applicants; // maps task id to applicants for the task
-    // @Adrian: why does task id have bytes32 and then uint64 format?
-    mapping(bytes32=>bool) internal isApplicant; // maps hash of task id and applicant address to boolean indicating whether the address corresponds to an applicant for the task
-    // @Adrian: probably again a mapping for the task contract?
-    mapping(uint64=>address) internal approved; // maps task id to a SINGLE approved applicant
     mapping(uint64=>address) internal assignment; // maps task id to a SINGLE assignee
 
     // @Adrian: this belongs here; managing tasks!
     mapping (address => uint64[]) organizerTasks; // maps organizer address to tasks that they are an organizer of
     mapping (address => uint64[]) workerTasks; // maps worker address to tasks that they are/were assigned to
 
-    ///@notice only allow the task assignee to call the function
-    modifier onlyAssignee(_id) {
-        require(assignment[_id] == msg.sender, "Callable: caller is not the task assignee");
-        _;
-    }
-
-    ///@notice only allow an approved applicant to call the function
-    modifier onlyApproved(_id) {
-        require(approved[_id] == msg.sender, "Callable: caller's application has not been approved");
-        _;
-    }
-
-    ///@notice constructor to instantiate the contract
-    ///@dev for now, all we have to do is set idCount to 0
-    constructor() {
-        // @Adrian: wouldn't id be implicitly managed through array position?
-       idCount = 0;
-    }
-
-    /* TO DO */
-    ///@notice internal function creating tasks for potential calls in other places
-    ///@dev may not need in the end 
-    function _createTask() internal {
-    }
-
-    /* TO DO */
-    ///@notice checks that owner is good for budget and witholds the budget
-    ///@dev this may end up being done completely in the PaymentSplitter?
-    function _withholdBudget() internal { 
-    }
-
     ///@notice creates a new task posting
     ///@dev tasks can only be created by an organizer
     ///@dev incrementing idCount depends on overflow protection; only use with Solidity verions >0.8!
-    function createTask(string memory _name, uint64 _budget, uint8 _time) public onlyOrganizer() {
+    function createTask(string memory _name, string memory _desc, uint64 _budget, uint8 _time) public onlyOrganizer() {
         idCount++;
-        tasks.push(Task(_name, idCount, _budget, _time, 0, false));
+        tasks.push( new Task(_name, _desc, idCount, _budget, _time, 0, false) );
         organizerTasks[msg.sender].push(idCount);
-        // call withholdBudget here
         emit TaskPosted(tasks.length-1);
     }
 
     ///@notice view ALL task postings, past and present
-    function viewAllPostings() public view returns(Task[] tasks){
+    function viewAllPostings() public view returns(Task[] _tasks){
         return tasks;
     }
 
     ///@notice view open postings that have not been assigned yet
-    function viewOpenPostings() public view returns(Task[] unassigned_tasks){
-        //for task in 
+    ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
+    function viewOpenPostings() public view returns(Task[] _unassignedTasks){
+        Task[] unassignedTasks;
+        for (uint i; i < tasks.length; i++) {
+            if (tasks[i].isAssigned) {
+                unassignedTasks.push(tasks[i]);
+            }
+        } 
+        return unassignedTasks;
     }
 
     ///@notice apply to accept a task
     function applyToTask(uint64 _id) public {
-        applicants[_id] = msg.sender;
-        keccak256(abi.encode(_id, msg.sender))] == 1
-        emit ApplicationCompleted(_id, msg.sender);
+        tasks[_id].apply(msg.sender);
+        emit TaskApplicationCompleted(_id, msg.sender);
     }
 
     ///@notice view applicants for a task
     ///@dev only an organizer of the task can do so
     ///@dev public view function -- no gas needed
     function viewApplicants(uint64 _id) public view onlyOrganizer() returns (address[] _applicants) {
-        return applicants[_id];
+        return tasks[_id].applicants;
     }
 
     //@notice accept task Applicant
     function acceptApplicant(uint64 _id, address _applicant) public onlyOrganizer() {
-        require(isApplicant[keccak256(abi.encode(_id, _applicant))] == 1, "Callable: input address is not an existing applicant and therefore cannot be accepted.");
-        approved[_id] = _applicant;
-        emit ApplicantAccepted(_id, _applicant);
+        tasks[_id].acceptApplicant(_applicant);
+        emit TaskApplicantAccepted(_id, _applicant);
     }
 
     ///@notice accept task assignment
     ///@dev tasks can only be accepted by approved applicants
-    function acceptAssignment(uint64 _id) public onlyApproved(_id) { 
-        assignment[_id] = msg.sender;
+    function acceptAssignment(uint64 _id) public { 
+        tasks[_id].acceptAssignment(msg.sender);
         workerTasks[msg.sender].push(_id);
         emit TaskAssigned(_id, msg.sender);
     }
