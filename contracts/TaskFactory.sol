5a26d5de8ceaf4a61fc11f24155f7bb0268aab26
_shashankN <36001641+ShashankNathani@users.noreply.github.com>
2022-05-11 14:25:25 -0400

    Update TaskFactory.sol

--- contracts/TaskFactory.sol
+++ contracts/TaskFactory.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.6;
 
 import "./OrganizationManager.sol";
 
-
-// task factory has one and only one use - to create tasks . No other functionality
+// task factory has only one job -> to create tasks
 
 contract TaskFactory is OrganizationManager { 
  
@@ -16,21 +15,38 @@ contract TaskFactory is OrganizationManager {
         uint64 public id;            
         uint64 public budgetPerUnit; //rate of the task
         uint16 public totalUnits;    
+        uint public uintsCompleted ;
+        address public organizer ;
+        address public assignee ;
         bool isAssigned ;
     }
 
     Task[] private tasks;
-    mapping (uint => address) public taskToOwner;
-    mapping (uint => address) public taskToContractor ;
+    mapping( uint => uint ) amountDue ;
     uint64 private idCount = 0 ;
+
+    ///@notice only allow the task assignee to call the function
+    modifier onlyAssignee(uint _taskId) {
+        require(tasks[_taskId].assignee == msg.sender, "Callable: caller is not the task assignee");
+        _;
+    }
+
+    modifier onlyTaskOrganizer(uint _taskId){
+        require(tasks[_taskId].organizer == msg.sender, "Callable: caller is not the task organizer");
+        _;
+    }
+
+    modifier onlyAssigneeOrTaskOrganizer(uint _taskId){
+        require(tasks[_taskId].organizer == msg.sender || tasks[_taskId].assignee == msg.sender , "Callable: caller is neither the task assignee nor the task organizer");
+        _;
+    }
 
 
     function createTask(string memory _name, string memory _desc, uint64 _budgetPerUnit, uint8 _totalUnits) 
         public 
         onlyOrganizer() 
     {
-        tasks.push( new Task(_name, _desc, idCount, _budgetPerUnit, _totalUnits,false,msg.sender)) ;
-        taskToOwner[idCount] = msg.sender;
+        tasks.push( new Task(_name, _desc, idCount, _budgetPerUnit, _totalUnits,0,msg.sender,address(0),false)) ;
         emit TaskPosted(tasks[idCount]);
         idCount++;
     }
@@ -42,15 +58,13 @@ contract TaskFactory is OrganizationManager {
         return tasks;
     }
 
-    function getTaskOwner(uint _id) 
+    function getTaskOwner(uint _taskId) 
         public 
         view returns(address)
     {
-        return taskToOwner[_id];
+        return tasks[_taskId].organizer ;
     }
 
-    ///@notice view open postings that have not been assigned yet
-    ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
 
     function viewOpenPostings() 
         public 
@@ -71,62 +85,27 @@ contract TaskFactory is OrganizationManager {
     {
         Task[] memory organizerTasks ;
         for (uint i; i < tasks.length; i++) {
-            if (taskToOwner[i] == msg.sender) {
+            if (tasks[i].organizer == msg.sender) {
                 organizerTasks.push(tasks[i]);
             }
         }
         return organizerTasks;
     }
 
-    ///@notice view worker tasks
-    ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
-    function viewContractorTasks()                                              // contractor can view all his tasks
+    function viewAssigneeTasks()                                              // contractor can view all his tasks
         public 
         view returns(Task[] memory _workerTasks)
     {
-        Task[] memory contractorTasks ;
+        Task[] memory assigneeTasks ;
         for (uint i; i < tasks.length; i++) {
-            if (taskToContractor[i] == msg.sender) {
-                contractorTasks.push(tasks[i]) ;
+            if (tasks[i].assignee == msg.sender) {
+                assigneeTasks.push(tasks[i]) ;
             }
         }
 
-        return contractorTasks;
+        return assigneeTasks;
     }
 
-
-
-    ///@notice apply to accept a task
-    // function applyToTask(uint64 _id) public {
-    //     tasks[_id].applyTo(msg.sender);
-    //     emit TaskApplicationCompleted(_id, msg.sender);
-    // }
-
-    ///@notice withdraw application to task
-    // function withdrawTaskApplication(uint64 _id) public {
-    //     tasks[_id].withdrawApplication(msg.sender);
-    //     emit TaskApplicationWithdrawn(_id, msg.sender);
-    // }
-
-    ///@notice view applicants for a task
-    ///@dev only an organizer of the task can do so
-    ///@dev public view function -- no gas needed
-    // function viewApplicants(uint64 _id) public view returns (address[] memory _applicants) {
-    //     return tasks[_id].viewApplicants();
-    // }
-
-    //@notice accept task Applicant
-    // function acceptTaskApplicant(uint64 _id, address _applicant) public {
-    //     tasks[_id].acceptApplicant(_applicant);
-    //     emit TaskApplicantAccepted(_id, _applicant);
-    // }
-
-    ///@notice accept task assignment
-    ///@dev tasks can only be accepted by approved applicants
-    // function acceptTaskAssignment(uint64 _id) public { 
-    //     tasks[_id].acceptAssignment(msg.sender);
-    //     workerTasks[msg.sender].push(_id);
-    //     emit TaskAssigned(_id, msg.sender);
-    // }
+    function getTaskId
 
 }
