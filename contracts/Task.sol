11517ee3c74a0656252322387fb8e7e7e1a99e1f
_shashankN <36001641+ShashankNathani@users.noreply.github.com>
2022-05-11 22:00:10 -0400

    Update Task.sol

--- contracts/Task.sol
+++ contracts/Task.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.6;
 
-import "./OrganizationManager.sol";
+import "./Payment.sol";
 
 // the script needs to include removal of assignees and they should not be included any further
 
@@ -13,8 +13,8 @@ contract Task is Payment { // also probably is Payable or whatever our payments 
     
     event Assignment(address _assignee);
     event RejectAssignment(address _approved);
-    event ProgressRequested(uint16 _addUnits, uint16 _completedUnits, uint16 _progressUnits);
-    event ProgressApproved(uint16 _addUnits, uint16 _completedUnits, uint16 _progressUnits);
+    event ProgressRequested(uint16 _addUnits, uint _completedUnits);
+    event ProgressApproved(uint16 _addUnits, uint _completedUnits)  ; 
     event Completion();
 
     mapping(uint => address[]) public applicants;                        // list of all applicants for that task   
@@ -23,21 +23,21 @@ contract Task is Payment { // also probably is Payable or whatever our payments 
 
 
     ///@notice only allow an approved applicant to call the function
-    function isApplicant(uint _taskId, address sender)              // whether the msg sender is the applicant for that id or not 
+    function isApplicant(uint _taskId, address _applicant)              // whether the msg sender is the applicant for that id or not 
         public
         view
-        returns (int index)
+        returns (int)
     {
-        for(int i=0;i<applicants[_taskId].length;i++){
-            if(applicants[_taskId][i]==sender){
-                return i ;
+        for(uint i=0; i < applicants[_taskId].length;i++){
+            if(applicants[_taskId][i] == _applicant){
+                return int(i) ;
             }
         }
         return -1 ;
     }
 
     modifier onlyApproved(uint _taskId) {                                                           // figure this out if needed or not
-        require(isApplicant(uint _taskId, msg.sender) != -1 , "Callable: caller's application has not been approved");
+        require(isApplicant(_taskId, msg.sender) != -1 , "Callable: caller's application has not been approved");
         _;
     }
 
@@ -54,21 +54,23 @@ contract Task is Payment { // also probably is Payable or whatever our payments 
     function withdrawApplication(uint _taskId) 
         public 
     {
-        int idx = isApplicant(_taskId,msg.sender) 
+        int idx = isApplicant(_taskId,msg.sender) ;
         require(idx == -1 ," Callabe: caller has not  applied for the task" ) ;
-        applicants[_taskId][idx] = address(0);
+        applicants[_taskId][uint(idx)] = address(0);
         emit ApplicationWithdrawn(msg.sender);
     }
 
     function viewApplicants(uint _taskId ) 
         public 
         view 
-        onlyTaskOrganizer() returns (address[] memory _currApplicants) 
+        onlyTaskOrganizer(_taskId) returns (address[] memory _currApplicants) 
     {
         address[] memory currApplicants ;
+        uint idx = 0; 
         for (uint i=0 ; i < applicants[_taskId].length; i++) {
             if (applicants[_taskId][i] != address(0) ) {
-                currApplicants.push(applicants[_taskId][i]);
+                currApplicants[idx] = applicants[_taskId][i];
+                idx++;
             }
         }
         return currApplicants;
@@ -77,54 +79,32 @@ contract Task is Payment { // also probably is Payable or whatever our payments 
     //@notice accept task Applicant
     function acceptApplicant(uint _taskId, address _applicant) 
         public 
-        onlyTaskOrganizer(uint _taskId) {
-        require(isApplicant(_tasks,_applicant) == -1, "Callable: input address is not an existing applicant and therefore cannot be accepted.");
+        onlyTaskOrganizer(_taskId) {
+        require(isApplicant(_taskId,_applicant) == -1, "Callable: input address is not an existing applicant and therefore cannot be accepted.");
         tasks[_taskId].assignee = _applicant ;
         emit ApplicantAccepted(_applicant);
     }
 
     function requestUpdate(uint _taskId, uint8 _addUnits) 
         public 
-        onlyAssignee(uint _taskId) 
+        onlyAssignee(_taskId) 
     { 
-        require( tasks[_taskId].completedUnits + _addUnits <= tasks[_taskId].totalUnits , "Additional progress units requested are more than the remaining units on the task." );
+        require( tasks[_taskId].uintsCompleted + _addUnits <= tasks[_taskId].totalUnits , "Additional progress units requested are more than the remaining units on the task." );
         require( _addUnits > 0 , "Additional progress units requested should be greater than 0." );
-        emit ProgressRequested(_addUnits, completedUnits, progressUnits);
+        emit ProgressRequested(_addUnits, tasks[_taskId].uintsCompleted);
     }
 
     function approveUpdate(uint _taskId, uint8 _addUnits) 
         public 
-        onlyTaskOrganizer(uint _taskId) 
+        onlyTaskOrganizer(_taskId) 
     { 
-        require( tasks[_taskId].completedUnits + _addUnits <= tasks[_taskId].totalUnits , "Additional progress units requested are more than the remaining units on the task." );
+        require( tasks[_taskId].uintsCompleted + _addUnits <= tasks[_taskId].totalUnits , "Additional progress units requested are more than the remaining units on the task." );
         require( _addUnits > 0 , "Additional progress units requested should be greater than 0." );
-        tasks[_taskId].completedUnits +=  _addUnits;
-        emit ProgressApproved(_addUnits, completedUnits, progressUnits);
-        if (tasks[_taskId].completedUnits == tasks[_taskId].totalUnits) {
+        tasks[_taskId].uintsCompleted +=  _addUnits;
+        emit ProgressApproved(_addUnits, tasks[_taskId].uintsCompleted);
+        if (tasks[_taskId].uintsCompleted == tasks[_taskId].totalUnits) {
             emit Completion();
         }
-    }
-
-    /**
-     * @dev Getter for the units completed
-     */
-    function getCompletedUnits() public view returns (uint64) {    // no req here
-        return unitsCompleted;     
-    }
-
-
-
-    function isCompleted() public view returns (bool) {         // no place here
-        return (unitsCompleted == totalUnits);
-    }
-
-
-    function getContractBalance() 
-        public 
-        view 
-        returns (uint256) 
-    {
-        return address(this).balance;
     }
 
 
