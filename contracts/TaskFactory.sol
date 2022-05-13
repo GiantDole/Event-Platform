138958ec5a6f4626060355ce6e3f1e633a9b1021
hannah-steele <55158545+hannah-steele@users.noreply.github.com>
2022-05-13 02:03:35 -0400

    Update TaskFactory.sol
    add TaskDetails struct -- may not be needed in the end

--- contracts/TaskFactory.sol
+++ contracts/TaskFactory.sol
@@ -29,6 +29,19 @@ contract TaskFactory is OrganizationManager {
     }
 
     Task[] private tasks;
+
+    struct TaskDetails {
+
+        string name;
+        string desc; // description of task
+        uint64 id; //id will be given through index in array -- tbd but we may not actually need this in the task
+        uint64 budgetPerUnit; //in wei?
+        uint16 progressUnits; // total units of progress that the task involves
+
+        uint16 completedUnits;
+        uint16 approvedUnits; // total units of progress that has been approved by the organizer for payment
+        bool isAssigned;
+    }
     
     // I don't actually think the below are needed -- use mappings to bool instead as we can loop through tasks array in a public view function that doesn't require gas.
     // uint64[] completedTasks; // array to store past tasks that are already completed (e.g. for tax purposes, etc.)
@@ -49,76 +62,91 @@ contract TaskFactory is OrganizationManager {
     ///@notice creates a new task posting
     ///@dev tasks can only be created by an organizer
     ///@dev incrementing idCount depends on overflow protection; only use with Solidity verions >0.8!
-    function createTask(string memory _name, string memory _desc, uint64 _budgetPerUnit, uint8 _progressUnits) public onlyOrganizer() {
+    function createTask(string memory _name, string memory _desc, uint64 _budgetPerUnit, uint8 _progressUnits) public onlyOrganizer() returns(uint64 _taskId){
         tasks.push( new Task(_name, _desc, idCount, _budgetPerUnit, _progressUnits) );
         tasks[idCount].transferOwnership(msg.sender);
         // organizerTasks[msg.sender].push(idCount);
         /// withhold budgetPerUnit * progressUnits here
         emit TaskPosted(tasks[idCount]);
         idCount++;
+        return idCount-1;
+    }
+
+    //@notice retrieve task details by id
+    function getTaskDetailsById(uint64 _id) public view returns(TaskDetails memory _taskDetails){
+        TaskDetails memory taskDetails = TaskDetails( 
+            tasks[_id].name(), tasks[_id].desc(), tasks[_id].id(), 
+            tasks[_id].budgetPerUnit(), tasks[_id].progressUnits(), tasks[_id].completedUnits(), tasks[_id].approvedUnits(), 
+            tasks[_id].isAssigned() 
+            );
+        return taskDetails;
+    }
+
+    function getTaskOwner(uint64 _id) public view returns(address){
+        return tasks[_id].owner();
     }
 
     ///@notice view ALL task postings, past and present
-    function viewAllPostings() public view returns(Task[] memory _tasks){
-        return tasks;
-    }
-
-    function getTaskOwner(uint _id) public view returns(address){
-        return tasks[_id].owner();
+    function viewAllPostings() public view returns(TaskDetails[] memory _taskDetailsArr){
+        TaskDetails[] memory taskDetailsArr = new TaskDetails[](tasks.length);
+        for (uint64 i; i < tasks.length; i++) {
+            taskDetailsArr[i] = getTaskDetailsById(i);
+        }
+        return taskDetailsArr;
     }
 
     ///@notice view open postings that have not been assigned yet
     ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
-    function viewOpenPostings() public view returns(Task[] memory _unassignedTasks){
-        Task[] memory filtTasks = new Task[](tasks.length);
-        uint16 currId = 0;
-        for (uint i; i < tasks.length; i++) {
+    function viewOpenPostings() public view returns(TaskDetails[] memory _unassignedTaskDetails){
+        TaskDetails[] memory filtArr = new TaskDetails[](tasks.length);
+        uint64 currId = 0;
+        for (uint64 i; i < tasks.length; i++) {
             if (tasks[i].isAssigned()) {
-                filtTasks[currId] = tasks[i];
+                filtArr[currId] = getTaskDetailsById(i);
                 currId++;
             }
         }
-        Task[] memory unassignedTasks = new Task[](currId+1);
-        for (uint j = 0; j < unassignedTasks.length; j++) {
-            unassignedTasks[j] = filtTasks[j];
+        TaskDetails[] memory unassignedTaskDetails = new TaskDetails[](currId+1);
+        for (uint64 j = 0; j < unassignedTaskDetails.length; j++) {
+            unassignedTaskDetails[j] = filtArr[j];
         }      
-        return unassignedTasks;
+        return unassignedTaskDetails;
     }
 
     ///@notice view organizer tasks
     ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
-    function viewOrganizerTasks() public view returns(Task[] memory _organizerTasks){
-        Task[] memory filtTasks = new Task[](tasks.length);
-        uint16 currId = 0;
-        for (uint i; i < tasks.length; i++) {
+    function viewOrganizerTasks() public view returns(TaskDetails[] memory _organizerTaskDetails){
+        TaskDetails[] memory filtArr = new TaskDetails[](tasks.length);
+        uint64 currId = 0;
+        for (uint64 i; i < tasks.length; i++) {
             if (tasks[i].isOrganizer(msg.sender)) {
-                filtTasks[currId] = tasks[i];
+                filtArr[currId] = getTaskDetailsById(i);
                 currId++;
             }
         }
-        Task[] memory organizerTasks = new Task[](currId);
-        for (uint j = 0; j < organizerTasks.length; j++) {
-            organizerTasks[j] = filtTasks[j];
+        TaskDetails[] memory organizerTaskDetails = new TaskDetails[](currId);
+        for (uint64 j = 0; j < organizerTaskDetails.length; j++) {
+            organizerTaskDetails[j] = filtArr[j];
         }      
-        return organizerTasks;
+        return organizerTaskDetails;
     }
 
     ///@notice view worker tasks
     ///@dev SHOULD THIS JUST BE DONE ON FRONT-END? I THINK MAYBE!!!
-    function viewWorkerTasks() public view returns(Task[] memory _workerTasks){
-        Task[] memory filtTasks = new Task[](tasks.length);
-        uint16 currId = 0;
-        for (uint i; i < tasks.length; i++) {
+    function viewWorkerTasks() public view returns(TaskDetails[] memory _workerTaskDetails){
+        TaskDetails[] memory filtArr = new TaskDetails[](tasks.length);
+        uint64 currId = 0;
+        for (uint64 i; i < tasks.length; i++) {
             if (tasks[i].assignment() == msg.sender) {
-                filtTasks[currId] = tasks[i];
+                filtArr[currId] = getTaskDetailsById(i);
                 currId++;
             }
         }
-        Task[] memory workerTasks = new Task[](currId);
-        for (uint j = 0; j < workerTasks.length; j++) {
-            workerTasks[j] = filtTasks[j];
+        TaskDetails[] memory workerTaskDetails = new TaskDetails[](currId);
+        for (uint64 j = 0; j < workerTaskDetails.length; j++) {
+            workerTaskDetails[j] = filtArr[j];
         }      
-        return workerTasks;
+        return workerTaskDetails;
     }
 
     function getContractAddress() public view returns(address contractAddress){
